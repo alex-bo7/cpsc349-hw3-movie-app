b5b4daff854c5454f5bd9d3b2c7b7cde9af3df78
alex-bo7 <anino882@gmail.com>
2025-10-12 19:31:54 -0700

    Implemented Title component

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+// 
+
+import Title from "./Title/Title"
+
+const App = () => {
+
+    return (
+        <>
+            <Title />
+            <p>app</p>
+        </>
+    )
+}
+
+export default App
---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,11 @@
+import "./title.css"
+
+const Title = () => {
+    return (
+        <div>
+            <h1 className="title-header">Movie Explorer</h1>
+        </div>
+    )
+}
+
+export default Title

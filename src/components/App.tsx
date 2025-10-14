ba65f4fb3ca3004f461f3cb1158edc189f948a2c
alex-bo7 <anino882@gmail.com>
2025-10-13 19:03:50 -0700

    HW2: API fetching

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,29 @@
-// 
-
+import { useEffect } from "react"
 import Filter from "./Filter/Filter"
 import Pagination from "./Pagination/Pagination"
 import Title from "./Title/Title"
 
+const TMDB_HTTPS = "https://api.themoviedb.org/3"
+
 const App = () => {
+
+    async function fetchMovies() {
+        const url = new URL(`${TMDB_HTTPS}/movie/popular`)
+        url.searchParams.set("api_key", import.meta.env.VITE_TMDB_API_KEY)
+
+        try {
+            const responce = await fetch(url)
+            const data = await responce.json()
+            console.log(data)
+        }
+        catch(error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchMovies()
+    }, [])
 
     return (
         <>
